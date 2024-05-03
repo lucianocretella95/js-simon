3164794274c5d31b3754e1d50cbd46996dc06cf0
lucianocretella95 <luciano.cretella@tim.it>
2024-05-03 16:29:35 +0200

    M1

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let numeri = document.getElementById("valori");
-for (let i = 1; i < 5; i++) {
+for (let i = 1; i <= 5; i++) {
   const element = i;
-  let number = Math.floor(Math.random() * 99) + 1;
+  let number = Math.floor(Math.random() * 99);
   console.log(number);
   numeri.innerHTML += `${number}`;
 }
